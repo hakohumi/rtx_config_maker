5f11f1719c3522ada862a3a81e231ef1cbda830d
hakohumi <mihumihu32@gmail.com>
2022-03-16 15:30:05 +0900

    replace to vue3 + vite

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
-<template>
-  <div id="app">
-    <HelloWorld />
-  </div>
-</template>
-
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
 </script>
+
+<template>
+  <HelloWorld msg="Hello vue3" />
+</template>
 
 <style>
 #app {
